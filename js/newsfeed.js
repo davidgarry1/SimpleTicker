64e979f3c80cbf3c42c92b0ab5e88bdad0e7a125
David Garry <dgarry@usc.edu>
2017-12-17 13:04:35 -0800

    infinite scroll

--- js/newsfeed.js
+++ js/newsfeed.js
@@ -1,31 +1,45 @@
 var dateObj = new Date();
-dateObj.setDate(dateObj.getDate() - 10);
+dateObj.setDate(dateObj.getDate() - 20);
 var month = dateObj.getUTCMonth() + 1; //months from 1-12
 var day = dateObj.getUTCDate();
 var year = dateObj.getUTCFullYear();
+var page = 1;
+var newsload = false;
+var PAGE_LIM = 10;
 
-var url = 'https://newsapi.org/v2/everything?' +
-          'sources=business-insider,bloomberg,the-wall-street-journal,cnbc,ars-technica,crypto-coins-news,engadget,recode,techradar,the-verge,wired,techcrunch,the-next-web,hacker-news,abc-news,cnn,associated-press,the-washington-post,the-huffington-post,usa-today&' +
-          'q=Cryptocurrency+Investing&' +
-          'from='+year+'-'+month+'-'+day+'&' +
-          'sortBy=popularity&' +
-          'language=en&' +
-          'apiKey=5448d59885724ecf9e865df785bbb667';
-
-var backupUrl = 'http://www.simpleticker.com/json/news.json';
-
-$.getJSON(url,function(raw){
-  console.log("Fetched newsfeed");
-  createArticleList(raw);
-}).fail(function(){
-  $.getJSON(backupUrl,function(raw){
-    console.log("Using cached newsfeed");
+function loadMoreNews(){
+  var url = 'https://newsapi.org/v2/everything?' +
+            'sources=business-insider,bloomberg,the-wall-street-journal,cnbc,ars-technica,crypto-coins-news,engadget,recode,techradar,the-verge,wired,techcrunch,the-next-web,hacker-news,abc-news,cnn,associated-press,the-washington-post,the-huffington-post,usa-today&' +
+            'page='+page+'&' +
+            'q=Cryptocurrency+Investing&' +
+            'from='+year+'-'+month+'-'+day+'&' +
+            'sortBy=popularity&' +
+            'language=en&' +
+            'apiKey=5448d59885724ecf9e865df785bbb667';
+  var backupUrl = 'http://www.simpleticker.com/json/news.json';
+  $.getJSON(url,function(raw){
+    console.log("Fetched newsfeed");
     createArticleList(raw);
+    PAGE_LIM = Math.min(PAGE_LIM, Math.floor(raw.totalResults/20)+1);
+    newsload = true;
   }).fail(function(){
-    console.log("Failed to load newsfeed");
-    showErrorLoadingNews();
+    if(page == 1){
+      $.getJSON(backupUrl,function(raw){
+        console.log("Using cached newsfeed");
+        createArticleList(raw);
+        newsload = true;
+      }).fail(function(){
+        console.log("Failed to load newsfeed");
+        showErrorLoadingNews();
+        newsload = true;
+      });
+    } else {
+      console.log("Failed to load newsfeed for page "+page);
+    }
   });
-});
+}
+
+loadMoreNews();
 
 function showErrorLoadingNews(){
   $("#newsfeed").html("<div class='article borderless'><div class='article-title'>Error loading newsfeed.<br><br>If this problem is persistent, please email us at <a target='_blank' href='mailto:bugs@simpleticker.com'>bugs@simpleticker.com</a>.</div></div>");
@@ -55,9 +69,19 @@ function createArticleList(news){
     article += "";
     output += article;
   }
-  output += "<div class='article article-inverted'><div class='news-title'>Recent News From <a target='_blank' href='https://newsapi.org/'>NewsAPI</a></div></div>";
-  $("#newsfeed").html(output);
+  $("#newsfeed").html(document.getElementById("newsfeed").innerHTML + output);
 }
+
+jQuery(function($) {
+    $('#sidebar').on('scroll', function() {
+        if($(this).scrollTop() + $(this).innerHeight() >= $(this)[0].scrollHeight) {
+            if(newsload && page <= PAGE_LIM){
+              page++;
+              loadMoreNews();
+            }
+        }
+    })
+});
 
 function convertTimestampToLocaleString(timeString){
   return new Date(timeString).toLocaleString('en-US');
