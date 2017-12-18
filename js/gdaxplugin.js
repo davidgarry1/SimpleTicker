29c74d05181721ca6b973730b92f68e486afcde1
David Garry <dgarry@usc.edu>
2017-12-17 19:54:11 -0800

    infinite scroll

--- js/gdaxplugin.js
+++ js/gdaxplugin.js
@@ -1,41 +1,41 @@
 var INTERVAL = 1500; //Rate limits available at https://docs.gdax.com/#rate-limits
 var CURRENT_COIN_NUM = 0;
 var HOME_CURRENCY = "USD";
-var GRANULARITY = 60*1000; //60 seconds
+var GRANULARITY = 60 * 1000; //60 seconds
 //Google Charts
 
 
 
-$("#cusd").click(function(){
-  HOME_CURRENCY = "USD";
-  $("#activec").html(HOME_CURRENCY);
-  updatePage(true);
-  setTimeout(function(){
-    updateCharts(true);
-  }, 500);
+$("#cusd").click(function() {
+    HOME_CURRENCY = "USD";
+    $("#activec").html(HOME_CURRENCY);
+    updatePage(true);
+    setTimeout(function() {
+        updateCharts(true);
+    }, 500);
 });
-$("#ceur").click(function(){
-  HOME_CURRENCY = "EUR";
-  $("#activec").html(HOME_CURRENCY);
-  updatePage(true);
-  setTimeout(function(){
-    updateCharts(true);
-  }, 500);
+$("#ceur").click(function() {
+    HOME_CURRENCY = "EUR";
+    $("#activec").html(HOME_CURRENCY);
+    updatePage(true);
+    setTimeout(function() {
+        updateCharts(true);
+    }, 500);
 });
-$("#cgbp").click(function(){
-  HOME_CURRENCY = "GBP";
-  $("#activec").html(HOME_CURRENCY);
-  updatePage(true);
-  setTimeout(function(){
-    updateCharts(true);
-  }, 500);
+$("#cgbp").click(function() {
+    HOME_CURRENCY = "GBP";
+    $("#activec").html(HOME_CURRENCY);
+    updatePage(true);
+    setTimeout(function() {
+        updateCharts(true);
+    }, 500);
 });
 
 function updateCoin(crypto, currency) {
     var min_frac = 2;
-    if((crypto == "ltc" || crypto == "eth") && currency == "GBP"){
-      currency = "BTC"; //no GBP exchange for LTC/ETH
-      min_frac = 7;
+    if ((crypto == "ltc" || crypto == "eth") && currency == "GBP") {
+        currency = "BTC"; //no GBP exchange for LTC/ETH
+        min_frac = 7;
     }
     var moneyFormatter = new Intl.NumberFormat("en-US", {
         style: "currency",
@@ -43,60 +43,60 @@ function updateCoin(crypto, currency) {
         minimumFractionDigits: min_frac,
         maximumFractionDigits: min_frac
     });
-    var percentFormatter = new Intl.NumberFormat("percent",{
+    var percentFormatter = new Intl.NumberFormat("percent", {
         style: "percent",
         minimumFractionDigits: 3,
         maximumFractionDigits: 3
     });
-    $.getJSON("https://api.gdax.com/products/"+crypto+"-"+currency+"/ticker", function(ticker) {
-        $("#"+crypto+"price").html(moneyFormatter.format(ticker.price));
-        if(crypto == "btc") document.title = moneyFormatter.format(ticker.price) + "-BTC | Simple Ticker";
-        $.getJSON("https://api.gdax.com/products/"+crypto+"-"+currency+"/stats", function(t) {
-            $("#"+crypto+"open").html(percentFormatter.format(((ticker.price/t.open)-1)));
-            changeColor(crypto+"price", ((ticker.price/t.open)-1));
-            changeColor(crypto+"open", ((ticker.price/t.open)-1));
-            $("#"+crypto+"high").html(moneyFormatter.format(t.high));
-            $("#"+crypto+"low").html(moneyFormatter.format(t.low));
-        }).fail(function(){
-          console.log("Too many requests to GDAX API");
+    $.getJSON("https://api.gdax.com/products/" + crypto + "-" + currency + "/ticker", function(ticker) {
+        $("#" + crypto + "price").html(moneyFormatter.format(ticker.price));
+        if (crypto == "btc") document.title = moneyFormatter.format(ticker.price) + "-BTC | Simple Ticker";
+        $.getJSON("https://api.gdax.com/products/" + crypto + "-" + currency + "/stats", function(t) {
+            $("#" + crypto + "open").html(percentFormatter.format(((ticker.price / t.open) - 1)));
+            changeColor(crypto + "price", ((ticker.price / t.open) - 1));
+            changeColor(crypto + "open", ((ticker.price / t.open) - 1));
+            $("#" + crypto + "high").html(moneyFormatter.format(t.high));
+            $("#" + crypto + "low").html(moneyFormatter.format(t.low));
+        }).fail(function() {
+            console.log("Too many requests to GDAX API");
         });
-    }).fail(function(){
-      console.log("Too many requests to GDAX API");
+    }).fail(function() {
+        console.log("Too many requests to GDAX API");
     });
 }
 
-function changeColor(elemID, num){
-  $("#"+elemID).removeClass("red");
-  $("#"+elemID).removeClass("green");
-  if(num >= 0){
-    $("#"+elemID).addClass("green");
-  } else {
-    $("#"+elemID).addClass("red");
-  }
+function changeColor(elemID, num) {
+    $("#" + elemID).removeClass("red");
+    $("#" + elemID).removeClass("green");
+    if (num >= 0) {
+        $("#" + elemID).addClass("green");
+    } else {
+        $("#" + elemID).addClass("red");
+    }
 }
 
-function updatePage(all){
-  times = 1;
-  if(all){
-    times = 3;
-  }
-  for(var i=0; i<times; i++){
-    if(++CURRENT_COIN_NUM>3){
-      CURRENT_COIN_NUM = 1;
+function updatePage(all) {
+    times = 1;
+    if (all) {
+        times = 3;
     }
-    if(CURRENT_COIN_NUM == 1){
-      updateCoin("btc",HOME_CURRENCY);
-    } else if(CURRENT_COIN_NUM == 2){
-      updateCoin("eth",HOME_CURRENCY);
-    } else {
-      updateCoin("ltc",HOME_CURRENCY);
+    for (var i = 0; i < times; i++) {
+        if (++CURRENT_COIN_NUM > 3) {
+            CURRENT_COIN_NUM = 1;
+        }
+        if (CURRENT_COIN_NUM == 1) {
+            updateCoin("btc", HOME_CURRENCY);
+        } else if (CURRENT_COIN_NUM == 2) {
+            updateCoin("eth", HOME_CURRENCY);
+        } else {
+            updateCoin("ltc", HOME_CURRENCY);
+        }
     }
-  }
 }
 
 updatePage(true);
-setTimeout(function(){
-  updateCharts(true);
+setTimeout(function() {
+    updateCharts(true);
 }, 1000);
 
 
@@ -104,32 +104,32 @@ setInterval(function() {
     updatePage(false);
 }, INTERVAL);
 
-$(window).resize(function(){
-  updateCharts(true);
+$(window).resize(function() {
+    updateCharts(true);
 });
 
 setInterval(function() {
-  updateCharts(false);
+    updateCharts(false);
 }, GRANULARITY);
 
-function updateCharts(hardReset){
-  if(hardReset){
-    firstBTCDraw = false;
-    firstETHDraw = false;
-    firstLTCDraw = false;
-  }
-  drawChart("btc", HOME_CURRENCY, hardReset);
-  drawChart("eth", HOME_CURRENCY, hardReset);
-  drawChart("ltc", HOME_CURRENCY, hardReset);
+function updateCharts(hardReset) {
+    if (hardReset) {
+        firstBTCDraw = false;
+        firstETHDraw = false;
+        firstLTCDraw = false;
+    }
+    drawChart("btc", HOME_CURRENCY, hardReset);
+    drawChart("eth", HOME_CURRENCY, hardReset);
+    drawChart("ltc", HOME_CURRENCY, hardReset);
 }
 
-function retryCharts(){
-    setTimeout(function(){
-      console.log("Attemping redraw");
-      if(!firstBTCDraw) drawChart("btc", HOME_CURRENCY, false);
-      if(!firstLTCDraw) drawChart("ltc", HOME_CURRENCY, false);
-      if(!firstETHDraw) drawChart("eth", HOME_CURRENCY, false);
-    },1500);
+function retryCharts() {
+    setTimeout(function() {
+        console.log("Attemping redraw");
+        if (!firstBTCDraw) drawChart("btc", HOME_CURRENCY, false);
+        if (!firstLTCDraw) drawChart("ltc", HOME_CURRENCY, false);
+        if (!firstETHDraw) drawChart("eth", HOME_CURRENCY, false);
+    }, 1500);
 }
 
 var firstBTCDraw = false;
@@ -138,93 +138,93 @@ var firstLTCDraw = false;
 
 
 function drawChart(crypto, currency, hardReset) {
-  if(hardReset){
-    document.getElementById(crypto+'chart').innerHTML = " Loading...";
-  }
+    if (hardReset) {
+        document.getElementById(crypto + 'chart').innerHTML = " Loading...";
+    }
 
-  if((crypto == "ltc" || crypto == "eth") && currency == "GBP"){
-    currency = "BTC"; //no GBP exchange for LTC/ETH
-  }
+    if ((crypto == "ltc" || crypto == "eth") && currency == "GBP") {
+        currency = "BTC"; //no GBP exchange for LTC/ETH
+    }
 
-$.getJSON("https://api.gdax.com/products/"+crypto+"-"+currency+"/candles?granularity="+GRANULARITY/1000, function(candles) {
-  console.log("Drawing "+crypto+"-"+currency+" chart");
-  if(crypto == "btc") firstBTCDraw = true;
-  if(crypto == "eth") firstETHDraw = true;
-  if(crypto == "ltc") firstLTCDraw = true;
-  var chartCandles = [];
+    $.getJSON("https://api.gdax.com/products/" + crypto + "-" + currency + "/candles?granularity=" + GRANULARITY / 1000, function(candles) {
+        console.log("Drawing " + crypto + "-" + currency + " chart");
+        if (crypto == "btc") firstBTCDraw = true;
+        if (crypto == "eth") firstETHDraw = true;
+        if (crypto == "ltc") firstLTCDraw = true;
+        var chartCandles = [];
 
-  for(var i=0; i<60; i++){
-    var time = candles[i][0];
-    var low = candles[i][1];
-    var high = candles[i][2];
-    var open = candles[i][3];
-    var close = candles[i][4];
-    var volume = candles[i][5];
-    chartCandles[i] = [0,0,0,0,0];
-    chartCandles[i][0] = new Date(time*1000);
-    chartCandles[i][1] = low;
-    chartCandles[i][2] = open;
-    chartCandles[i][3] = close;
-    chartCandles[i][4] = high;
-  }
+        for (var i = 0; i < 60; i++) {
+            var time = candles[i][0];
+            var low = candles[i][1];
+            var high = candles[i][2];
+            var open = candles[i][3];
+            var close = candles[i][4];
+            var volume = candles[i][5];
+            chartCandles[i] = [0, 0, 0, 0, 0];
+            chartCandles[i][0] = new Date(time * 1000);
+            chartCandles[i][1] = low;
+            chartCandles[i][2] = open;
+            chartCandles[i][3] = close;
+            chartCandles[i][4] = high;
+        }
 
 
-    var data = google.visualization.arrayToDataTable(chartCandles, true);// Treat first row as data as well.
+        var data = google.visualization.arrayToDataTable(chartCandles, true); // Treat first row as data as well.
 
-    var options = {
-        legend: 'none',
-        bar: {
-            groupWidth: '100%'
-        }, // Remove space between bars.
-        colors: ['lightgrey'],
-        candlestick: {
-            fallingColor: {
-                stroke: 'black',
-                strokeWidth: 0,
-                fill: '#a52714'
-            }, // red
-            risingColor: {
-                stroke: 'black',
-                strokeWidth: 0,
-                fill: '#0f9d58'
-            }, // green
-        },
-        chartArea: {
-            left: '8%',
-            bottom: '10%',
-            width: '91%',
-            height: '85%',
-        },
-        vAxis: {
-
-            titleTextStyle: {
-                color: 'black'
+        var options = {
+            legend: 'none',
+            bar: {
+                groupWidth: '100%'
+            }, // Remove space between bars.
+            colors: ['lightgrey'],
+            candlestick: {
+                fallingColor: {
+                    stroke: 'black',
+                    strokeWidth: 0,
+                    fill: '#a52714'
+                }, // red
+                risingColor: {
+                    stroke: 'black',
+                    strokeWidth: 0,
+                    fill: '#0f9d58'
+                }, // green
             },
-            gridlines: {
-                color: 'lightrey',
-
+            chartArea: {
+                left: '8%',
+                bottom: '10%',
+                width: '91%',
+                height: '85%',
             },
-        },
+            vAxis: {
 
-        backgroundColor: 'white',
-        fontName: 'Consolas',
-        hAxis: {
-            logscale: false
-        },
-        hAxis: {
-            gridlines: {
-                color: 'lightgrey',
+                titleTextStyle: {
+                    color: 'black'
+                },
+                gridlines: {
+                    color: 'lightrey',
 
+                },
             },
 
-        },
-    };
+            backgroundColor: 'white',
+            fontName: 'Consolas',
+            hAxis: {
+                logscale: false
+            },
+            hAxis: {
+                gridlines: {
+                    color: 'lightgrey',
 
-    var chart = new google.visualization.CandlestickChart(document.getElementById(crypto+'chart'));
+                },
 
-    chart.draw(data, options);
-}).fail(function(){
-    console.log("Chart API Call Failed");
-    retryCharts();
-});
+            },
+        };
+
+        var chart = new google.visualization.CandlestickChart(document.getElementById(crypto + 'chart'));
+
+        chart.draw(data, options);
+    }).fail(function() {
+        console.log("Chart API Call Failed");
+        retryCharts();
+    });
 }
--- js/newsfeed.js
+++ js/newsfeed.js
@@ -7,82 +7,82 @@ var page = 1;
 var newsload = false;
 var PAGE_LIM = 10;
 
-function loadMoreNews(){
-  var url = 'https://newsapi.org/v2/everything?' +
-            'sources=business-insider,bloomberg,the-wall-street-journal,cnbc,ars-technica,crypto-coins-news,engadget,recode,techradar,the-verge,wired,techcrunch,the-next-web,hacker-news,abc-news,cnn,associated-press,the-washington-post,the-huffington-post,usa-today&' +
-            'page='+page+'&' +
-            'q=Cryptocurrency+Investing&' +
-            'from='+year+'-'+month+'-'+day+'&' +
-            'sortBy=popularity&' +
-            'language=en&' +
-            'apiKey=5448d59885724ecf9e865df785bbb667';
-  var backupUrl = 'http://www.simpleticker.com/json/news.json';
-  $.getJSON(url,function(raw){
-    console.log("Fetched newsfeed");
-    createArticleList(raw);
-    PAGE_LIM = Math.min(PAGE_LIM, Math.floor(raw.totalResults/20)+1);
-    newsload = true;
-  }).fail(function(){
-    if(page == 1){
-      $.getJSON(backupUrl,function(raw){
-        console.log("Using cached newsfeed");
+function loadMoreNews() {
+    var url = 'https://newsapi.org/v2/everything?' +
+        'sources=business-insider,bloomberg,the-wall-street-journal,cnbc,ars-technica,crypto-coins-news,engadget,recode,techradar,the-verge,wired,techcrunch,the-next-web,hacker-news,abc-news,cnn,associated-press,the-washington-post,the-huffington-post,usa-today&' +
+        'page=' + page + '&' +
+        'q=Cryptocurrency+Investing&' +
+        'from=' + year + '-' + month + '-' + day + '&' +
+        'sortBy=popularity&' +
+        'language=en&' +
+        'apiKey=5448d59885724ecf9e865df785bbb667';
+    var backupUrl = 'http://www.simpleticker.com/json/news.json';
+    $.getJSON(url, function(raw) {
+        console.log("Fetched newsfeed");
         createArticleList(raw);
+        PAGE_LIM = Math.min(PAGE_LIM, Math.floor(raw.totalResults / 20) + 1);
         newsload = true;
-      }).fail(function(){
-        console.log("Failed to load newsfeed");
-        showErrorLoadingNews();
-        newsload = true;
-      });
-    } else {
-      console.log("Failed to load newsfeed for page "+page);
-    }
-  });
+    }).fail(function() {
+        if (page == 1) {
+            $.getJSON(backupUrl, function(raw) {
+                console.log("Using cached newsfeed");
+                createArticleList(raw);
+                newsload = true;
+            }).fail(function() {
+                console.log("Failed to load newsfeed");
+                showErrorLoadingNews();
+                newsload = true;
+            });
+        } else {
+            console.log("Failed to load newsfeed for page " + page);
+        }
+    });
 }
 
 loadMoreNews();
 
-function showErrorLoadingNews(){
-  $("#newsfeed").html("<div class='article borderless'><div class='article-title'>Error loading newsfeed.<br><br>If this problem is persistent, please email us at <a target='_blank' href='mailto:bugs@simpleticker.com'>bugs@simpleticker.com</a>.</div></div>");
+function showErrorLoadingNews() {
+    $("#newsfeed").html("<div class='article borderless'><div class='article-title'>Error loading newsfeed.<br><br>If this problem is persistent, please email us at <a target='_blank' href='mailto:bugs@simpleticker.com'>bugs@simpleticker.com</a>.</div></div>");
 }
 
-function createArticleList(news){
-  var output = "";
-  for(var i=0; i<news.articles.length; i++){
-    var article = "";
-    if(i!= news.articles.length-1){
-      article += "<div class='article border-bottom'>";
-    }else{
-      article += "<div class='article'>";
+function createArticleList(news) {
+    var output = "";
+    for (var i = 0; i < news.articles.length; i++) {
+        var article = "";
+        if (i != news.articles.length - 1) {
+            article += "<div class='article border-bottom'>";
+        } else {
+            article += "<div class='article'>";
+        }
+        article += "<a target='_blank' href='";
+        article += news.articles[i].url;
+        article += "' title='";
+        article += news.articles[i].description;
+        article += "'>";
+        article += "<div class='article-title'>";
+        article += news.articles[i].title;
+        article += "</div></a><div class='article-meta'><span class='article-sitename'>";
+        article += news.articles[i].source.name;
+        article += "</span>&nbsp&nbsp<span class='article-time'>";
+        article += convertTimestampToLocaleString(news.articles[i].publishedAt);
+        article += "</span></div></div>";
+        article += "";
+        output += article;
     }
-    article += "<a target='_blank' href='";
-    article += news.articles[i].url;
-    article += "' title='";
-    article += news.articles[i].description;
-    article += "'>";
-    article +="<div class='article-title'>";
-    article += news.articles[i].title;
-    article += "</div></a><div class='article-meta'><span class='article-sitename'>";
-    article += news.articles[i].source.name;
-    article += "</span>&nbsp&nbsp<span class='article-time'>";
-    article += convertTimestampToLocaleString(news.articles[i].publishedAt);
-    article += "</span></div></div>";
-    article += "";
-    output += article;
-  }
-  $("#newsfeed").html(document.getElementById("newsfeed").innerHTML + output);
+    $("#newsfeed").html(document.getElementById("newsfeed").innerHTML + output);
 }
 
 jQuery(function($) {
     $('#sidebar').on('scroll', function() {
-        if($(this).scrollTop() + $(this).innerHeight() >= $(this)[0].scrollHeight) {
-            if(newsload && page <= PAGE_LIM){
-              page++;
-              loadMoreNews();
+        if ($(this).scrollTop() + $(this).innerHeight() >= $(this)[0].scrollHeight) {
+            if (newsload && page <= PAGE_LIM) {
+                page++;
+                loadMoreNews();
             }
         }
     })
 });
 
-function convertTimestampToLocaleString(timeString){
-  return new Date(timeString).toLocaleString('en-US');
+function convertTimestampToLocaleString(timeString) {
+    return new Date(timeString).toLocaleString('en-US');
 }
